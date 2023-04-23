5b96ddd16120aad129d2ee38e8bbc2208b86f990
Lee Chapman <me@leejonchapman.com>
2023-04-23 20:37:45 +0100

    pass

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        C c = new C();
+
+        c.a();
+        c.b();
+        c.c();
     }
 }
